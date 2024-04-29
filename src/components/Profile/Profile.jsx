6b23d2dd9971bf0e81f2a7e340e8a4c0ca9033af
itakash1 <itakashh11@gmail.com>
2024-04-29 15:57:30 +0400

    added profile posts

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,21 +11,30 @@ export default function Profile() {
                     <img className='banner'src={banner} alt=""/>
                 </div>
             </div>
+
             <div className="profile-data">
-                <h1 className="name">
-                    <span>Nikolay</span>
-                    <span>Malikov</span>
-                </h1>
-                <p className="status">
-                    <span>Write some about ur</span>
-                </p>
+                <div className="profile-title">
+                    <h1 className="name">
+                        <span>Nikolay Malikov</span>
+                    </h1>
+                    <p className="status">
+                        <span>Write some about ur</span>
+                    </p>
+                </div>
             </div>
             <div className="profile-wall">
-                <div className="post2">
-                    <p>Im so lazy... But im gtg teach React!</p>
-                </div>
-                <div className="post1">
-                    <p>Hello everybody is my first post on this social web!</p>
+                <div className="post">
+                    <div className="title-post">
+                        <div className='avatar-user-post'>
+                            <img src={avatar} alt="avatar"/>
+                        </div>
+                        <div className="name-user-post">
+                            <span>Nikolay Malikov</span>
+                        </div>
+                    </div>
+                    <div className='data-post'>
+                        <p>Im so lazy... But im gtg teach React!</p>
+                    </div>
                 </div>
             </div>
         </div>
