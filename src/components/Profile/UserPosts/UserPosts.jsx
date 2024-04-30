5ab2cc21bb663bcdab37eb02b962f90a02c788b1
itakash1 <itakashh11@gmail.com>
2024-04-30 22:43:24 +0400

    change structure post component

--- src/components/Profile/UserPosts/UserPosts.jsx
+++ src/components/Profile/UserPosts/UserPosts.jsx
@@ -3,21 +3,21 @@ import avatar from "../../../assets/avatar.jpeg";
 
 export default function UserPosts() {
     return (
-        <div className={css.profile_wall}>
-            <div className={css.input_void}>
-                <input type="text" placeholder="What do u think about z1trxks?"/>
+        <div className={css.wall__post__void}>
+            <div className={css.input__void}>
+                <textarea placeholder="What do u think about z1trxks?"/>
                 <button>Send</button>
             </div>
-            <div className={css.post}>
-                <div className={css.title_post}>
-                    <div className={css.avatar_user_post}>
+            <div className={css.user__post}>
+                <div className={css.user__post_title}>
+                    <div className={css.user__post_avatar}>
                         <img src={avatar} alt="avatar"/>
                     </div>
-                    <div className={css.name_user_post}>
+                    <div className={css.user__post_name}>
                         <span>Nikolay Malikov</span>
                     </div>
                 </div>
-                <div className={css.data_post}>
+                <div className={css.user__post_data}>
                     <p>Im so lazy... But im gtg teach React!</p>
                 </div>
             </div>
