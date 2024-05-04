61cc11a2baf8a8113a68fef83ad868962a47e187
itakash1 <itakashh11@gmail.com>
2024-05-04 17:53:23 +0400

    colored active link

--- src/components/MainPage/Navbar/Navbar.jsx
+++ src/components/MainPage/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import css from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export default function Navbar() {
+    const  activeTagName = (data) => data.isActive ? `${css.active} ${css.item}` : css.item;
     return (
-        <nav className={css.navbar}>
-                <a className={`${css.item} ${css.active}`} href="/profile">Profile</a>
-                <a className={css.item} href="/dialogs">Chats</a>
-                <a className={css.item} href="#">Music</a>
-                <a className={css.item} href="#">News</a>
+        <nav className={`${css.navbar}`}>
+                <NavLink className={activeTagName} to="/profile">Profile</NavLink>
+                <NavLink className={activeTagName} to="/dialogs">Chats</NavLink>
+                <NavLink className={css.item} to="#">Music</NavLink>
+                <NavLink className={css.item} to="#">News</NavLink>
         </nav>
     );
 }
