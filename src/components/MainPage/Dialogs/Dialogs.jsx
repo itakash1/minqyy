e2a17274613d4add7710337ba2cc4146ab16589b
itakash1 <itakashh11@gmail.com>
2024-05-09 01:33:07 +0400

    ready to maps

--- src/components/MainPage/Dialogs/Dialogs.jsx
+++ src/components/MainPage/Dialogs/Dialogs.jsx
@@ -4,18 +4,31 @@ import Messagease from "./Messagease/Messagease";
 import avatar from "../../../assets/user-avatar.png";
 
 export default function Dialogs(props){
+    let arrayUsers = [
+        {id: 1, name: "Андрюха Лупатый", avatar: avatar,},
+        {id: 2, name: "Николай Турбо", avatar: avatar,},
+        {id: 3, name: "Карась Фандю", avatar: avatar,},
+        {id: 4, name: "Дмитрий Бычок", avatar: avatar,},
+    ]
+
+    let msgData = [
+        {id: 1, msg: "hello",},
+        {id: 1, msg: "budlo",},
+        {id: 1, msg: "ya tut",},
+        {id: 1, msg: "poshli v dotu",},
+    ]
     return(
         <div className={css.dialogs}>
             <div className={css.dialogs_users_nav}>
-                <Users name="Николай Турбо" avatar={avatar} id={0}/>
-                <Users name="Андрей Бабуин" avatar={avatar} id={1}/>
-                <Users name="Карась Фандю" avatar={avatar} id={2}/>
-                <Users name="Дмитрий Бычок" avatar={avatar} id={3}/>
+                <Users name={arrayUsers[0].name} avatar={arrayUsers[0].avatar} id={arrayUsers[0].id}/>
+                <Users name={arrayUsers[1].name} avatar={arrayUsers[1].avatar} id={arrayUsers[1].id}/>
+                <Users name={arrayUsers[2].name} avatar={arrayUsers[2].avatar} id={arrayUsers[2].id}/>
+                <Users name={arrayUsers[3].name} avatar={arrayUsers[3].avatar} id={arrayUsers[3].id}/>
             </div>
             <div className={css.dialog_chat_void}>
-                <Messagease msg={'Привет ебанат'}/>
-                <Messagease msg={'Я его все ебал'}/>
-                <Messagease msg={'Айм файн нахуй'}/>
+                <Messagease msg={msgData[0].msg}/>
+                <Messagease msg={msgData[1].msg}/>
+                <Messagease msg={msgData[2].msg}/>
 
                 <div className={css.dialog_free_void}>
 
--- src/components/MainPage/Dialogs/Users/Users.jsx
+++ src/components/MainPage/Dialogs/Users/Users.jsx
@@ -1,8 +1,10 @@
 import css from "./Users.module.css";
 import {NavLink} from "react-router-dom";
+import avatar from "../../../../assets/user-avatar.png";
 
 export default function Users(props){
     const  activeTagName = (data) => data.isActive ? `${css.chat_user} ${css.active}` : css.chat_user;
+
     return (
         <NavLink to={`/dialogs/id${props.id}`} className={activeTagName}>
             <img src={props.avatar} alt=""/>
