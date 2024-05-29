0b4b546cc50aeaf3ddb3e0438323a7f747288fcc
itakash1 <itakashh11@gmail.com>
2024-05-29 13:06:21 +0400

    start time

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import StoreContext from "./StoreContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 export let renderTree = () => {
     root.render(
         <React.StrictMode>
-            <StoreContext.Provider value={store}>
-                <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
-            </StoreContext.Provider>
+            <App state={store.getState()} dispatch={store.dispatch.bind(store)} />
         </React.StrictMode>
     );
 }
